2a294c6b7f1bd4176ad9c7ce220e8b471d2d4dd3
Jesse Kane <jessekane66@gmail.com>
2025-02-06 20:02:52 -0500

    recent commitrs

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025-Competition-Neo";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "cd8e8784c72d10fe858ef4927f25d935041be0e5";
+  public static final String GIT_DATE = "2025-01-17 17:46:23 EST";
+  public static final String GIT_BRANCH = "simulation";
+  public static final String BUILD_DATE = "2025-01-17 17:56:38 EST";
+  public static final long BUILD_UNIX_TIME = 1737154598667L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
